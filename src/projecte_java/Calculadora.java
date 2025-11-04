c911e6a374af473f4a625ea0e1425f17ca0c3783
paunadal14-web <paunadal14@alumnes.ilerna.com>
2025-11-04 21:58:54 +0100

    Calculadora java
    Calculadora java

---
+++ src/projecte_java/Calculadora.java
@@ -0,0 +1,15 @@
+package projecte_java;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println("=== Calculadora Básica ===");
+        int a = 10;
+        int b = 5;
+ 
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Resta: " + (a - b));
+        System.out.println("Multiplicación: " + (a * b));
+        System.out.println("División: " + (a / b));
+    }
+}
+
